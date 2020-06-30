7d9567d36c987a3a1beb88472a9171daa9da28ed
James Ambrose <galaxy2198@gmail.com>
2020-06-30 08:05:10 -0500

    added variable host

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -2,5 +2,6 @@
 
 sed -i 's/__SSH_USER/'"$SSH_USER"'/g' /ansible/inventory
 sed -i 's/__SSH_PASS/'"$SSH_PASS"'/g' /ansible/inventory
+sed -i 's/__SSH_HOST/'"$SSH_HOST"'/g' /ansible/inventory
 
 ansible-playbook -i /ansible/inventory /ansible/playbook.yml
